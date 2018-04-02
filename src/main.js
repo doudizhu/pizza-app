2ef3ed383111cd6a1333d6fd7a7f7e76eec1d81c
lipeng <lipeng@daokoudai.com>
2018-04-02 19:58:22 +0800

    install and config element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 1.引入样式element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
