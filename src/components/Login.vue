596521177e85e833d78872016383ca0eac6466e1
lipeng <lipeng@daokoudai.com>
2018-04-03 17:53:31 +0800

    登录和注册页面搭建

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,28 @@
 <template lang="pug">
-	h1 登录
+	div
+		h1 登录
+		el-form(@submit.prevent='onSubmit')
+			el-form-item(label='邮箱')
+				el-input(v-model='email')
+			el-form-item(label='密码')
+				el-input(v-model='password')
+			el-form-item
+				el-button(type='submit' @click='onSubmit') 登录
+
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
+</script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,3 +1,31 @@
 <template lang="pug">
-	h1 注册
+	div
+		h1 注册
+		el-form(@submit.prevent='onSubmit')
+			el-form-item(label='邮箱')
+				el-input(v-model='email')
+			el-form-item(label='密码')
+				el-input(v-model='password')
+			el-form-item(label='确认密码')
+				el-input(v-model='confirmPassword')
+			el-form-item
+				el-button(type='submit' @click='onSubmit') 注册
+
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }
+    },
+    methods: {
+      onSubmit() {
+        
+      }
+    }
+  }
+</script>
