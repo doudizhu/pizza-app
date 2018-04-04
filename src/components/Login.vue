031e748c6c31d2212dd1430abd536dd687ce2ea1
lipeng <lipeng@daokoudai.com>
2018-04-04 09:54:16 +0800

    使用axios实现登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
   export default {
     data() {
       return {
@@ -21,7 +23,32 @@
     },
     methods: {
       onSubmit() {
-        console.log('submit!');
+        axios.get('/users.json')
+			.then(res=>{
+				// console.log(res);
+				const data = res.data
+				const users = []
+				for(let key in data){
+					const user = data[key]
+					// console.log(user)
+					users.push(user)
+				}
+
+				// console.log(users);
+				
+				// 实现过滤
+				let result = users.filter(user=>{
+					return user.email === this.email && user.password === this.password
+				})
+
+				// console.log(result);
+				// 判断result的长度是否大于0
+				if(result != null && result.length >0){
+					this.$router.push({name:'homeLink'})
+				}else{
+					alert('账号或密码错误')
+				}
+			})
       }
     }
   }
